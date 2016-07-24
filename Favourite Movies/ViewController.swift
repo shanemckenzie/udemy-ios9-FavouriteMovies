df31a00ae6a6f1c20b7448511dc25c9a47e0c54d
Shane McKenzie <shane.mckenzie59@gmail.com>
2016-07-24 11:27:22 -0600

    Initial Commit

---
+++ Favourite Movies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Favourite Movies
+//
+//  Created by Shane Mckenzie on 7/24/16.
+//  Copyright © 2016 Shane Mckenzie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
